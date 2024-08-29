851d4f190278c37af822b6d84ac9955427c1d6ae
pita <154020297+pita092@users.noreply.github.com>
2024-08-29 16:44:24 +0300

    Update cmp.lua

--- lua/nvchad/configs/cmp.lua
+++ lua/nvchad/configs/cmp.lua
@@ -1,150 +1,117 @@
+local cmp = require "cmp"
+
 dofile(vim.g.base46_cache .. "cmp")
-  require('nvim-autopairs').setup {}
-local luasnip = require 'luasnip'
-local cmp = require 'cmp'
-local cmp_action = require('lsp-zero').cmp_action()
-local kind_icons = {
-  Text = '󰉿',
-  Method = '󰆧',
-  Function = '󰊕',
-  Constructor = '',
-  Field = '󰜢',
-  Variable = '󰀫',
-  Class = '󰠱',
-  Interface = '',
-  Module = '',
-  Property = '󰜢',
-  Unit = '󰑭',
-  Value = '󰎠',
-  Enum = '',
-  Keyword = '󰌋',
-  Snippet = '',
-  Color = '󰏘',
-  File = '󰈙',
-  Reference = '󰈇',
-  Folder = '󰉋',
-  EnumMember = '',
-  Constant = '󰏿',
-  Struct = '󰙅',
-  Event = '',
-  Operator = '󰆕',
-  TypeParameter = '',
+
+local cmp_ui = require("nvconfig").ui.cmp
+local cmp_style = cmp_ui.style
+
+local field_arrangement = {
+  atom = { "kind", "abbr", "menu" },
+  atom_colored = { "kind", "abbr", "menu" },
 }
-local fuzzy_path_option = {
-  fd_cmd = {
-    vim.fn.executable 'fd' == 1 and 'fd' or 'fdfind',
-    '-p',
-    '-H',
-    '-L',
-    '-td',
-    '-tf',
-    '-tl',
-    '--mount',
-    '-c=never',
-    '-E=*.git/',
-    '-E=*.venv/',
-    '-E=*Cache*/',
-    '-E=*cache*/',
-    '-E=.*Cache*/',
-    '-E=.*cache*/',
-    '-E=.*wine/',
-    '-E=.cargo/',
-    '-E=.conda/',
-    '-E=.dot/',
-    '-E=.fonts/',
-    '-E=.ipython/',
-    '-E=.java/',
-    '-E=.jupyter/',
-    '-E=.luarocks/',
-    '-E=.mozilla/',
-    '-E=.npm/',
-    '-E=.nvm/',
-    '-E=.steam*/',
-    '-E=.thunderbird/',
-    '-E=.tmp/',
-    '-E=__pycache__/',
-    '-E=dosdevices/',
-    '-E=node_modules/',
-    '-E=vendor/',
-    '-E=venv/',
+
+local formatting_style = {
+  -- default fields order i.e completion word + item.kind + item.kind icons
+  fields = field_arrangement[cmp_style] or { "abbr", "kind", "menu" },
+
+  format = function(_, item)
+    local icons = require "nvchad.icons.lspkind"
+    local icon = (cmp_ui.icons and icons[item.kind]) or ""
+
+    if cmp_style == "atom" or cmp_style == "atom_colored" then
+      icon = " " .. icon .. " "
+      item.menu = cmp_ui.lspkind_text and "   (" .. item.kind .. ")" or ""
+      item.kind = icon
+    else
+      icon = cmp_ui.lspkind_text and (" " .. icon .. " ") or icon
+      item.kind = string.format("%s %s", icon, cmp_ui.lspkind_text and item.kind or "")
+    end
+
+    return item
+  end,
+}
+
+local function border(hl_name)
+  return {
+    { "╭", hl_name },
+    { "─", hl_name },
+    { "╮", hl_name },
+    { "│", hl_name },
+    { "╯", hl_name },
+    { "─", hl_name },
+    { "╰", hl_name },
+    { "│", hl_name },
+  }
+end
+
+local options = {
+  completion = {
+    completeopt = "menu,menuone",
   },
-}
-local borderstyle = {
-  border = { '┌', '─', '┐', '│', '┘', '─', '└', '│' },
-  winhighlight = 'Normal:CmpPmenu,CursorLine:CmpSel,Search:None',
-}
 
-require('luasnip.loaders.from_vscode').lazy_load()
-require('luasnip').filetype_extend('c', { 'cdoc' })
-
-cmp.setup {
+  window = {
+    completion = {
+      side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
+      winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
+      scrollbar = false,
+    },
+    documentation = {
+      border = border "CmpDocBorder",
+      winhighlight = "Normal:CmpDoc",
+    },
+  },
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      require("luasnip").lsp_expand(args.body)
     end,
   },
-  window = {
-    completion = borderstyle,
-    documentation = borderstyle,
-  },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<Tab>'] = cmp_action.luasnip_supertab(),
-    ['<C-Tab>'] = cmp_action.luasnip_shift_supertab(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm { select = true },
-    ['<C-l>'] = cmp.mapping(function()
-      if luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      end
-    end, { 'i', 's' }),
-    ['<C-h>'] = cmp.mapping(function()
-      if luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      end
-    end, { 'i', 's' }),
-  },
 
+  formatting = formatting_style,
+
+  mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+
+    ["<CR>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    },
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif require("luasnip").expand_or_jumpable() then
+        require("luasnip").expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif require("luasnip").jumpable(-1) then
+        require("luasnip").jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  },
   sources = {
-    { name = 'luasnip', max_item_count = 3 },
-    { name = 'nvim_lsp_signature_help' },
-    { name = 'nvim_lsp', max_item_count = 20 },
-    {
-      name = 'buffer',
-      max_item_count = 8,
-      option = {},
-    },
-    { name = 'path' },
-    {
-      name = 'fuzzy_path',
-      option = fuzzy_path_option,
-      entry_filter = function()
-        return vim.g.loaded_vimtex ~= 1 or vim.bo.ft ~= 'markdown' and vim.bo.ft ~= 'tex' or vim.api.nvim_eval 'vimtex#syntax#in_mathzone()' ~= 1
-      end,
-    },
-  },
-  formatting = {
-    fields = { 'abbr', 'kind', 'menu' },
-    expandable_indicator = true,
-    format = function(entry, vim_item)
-      local kind = require('lspkind').cmp_format { mode = 'text', maxwidth = 50 }(entry, vim_item)
-      local strings = vim.split(kind.kind, ' ', { trimempty = true })
-      kind.kind = string.format('%s  %s', kind_icons[vim_item.kind], strings[1])
-      vim.api.nvim_set_hl(0, 'CmpSel', { bg = '#fbf1c7', fg = '#282828' })
-      -- This concatenates the icons with the name of the item kind
-
-      -- NOTE: Don't remove the line below if you don't want the CMP to go haywire
-      kind.menu = '' .. (strings[2] or '') .. ''
-
-      return kind
-    end,
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "nvim_lua" },
+    { name = "path" },
   },
 }
-cmp.setup.cmdline('@', { enabled = false })
-cmp.setup.cmdline('>', { enabled = false })
-cmp.setup.cmdline('-', { enabled = false })
-cmp.setup.cmdline('=', { enabled = false })
 
+if cmp_style ~= "atom" and cmp_style ~= "atom_colored" then
+  options.window.completion.border = border "CmpBorder"
 end
+
+return options
